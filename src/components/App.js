da0f59b8c0b54b9da37c5255a62c2f04e7ef1016
abdulrawoofali <43907143+abdulrawoofali@users.noreply.github.com>
2020-11-24 21:31:05 +0530

    removed-redux

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,75 @@
-import React from "react";
-import MySlide from "./MySlideView/MySlide";
-import ButtonList from "./ButtonList/ButtonList";
+import React, { Component, useState } from "react";
 import "../styles/App.css";
+const [NEXT, PREV, RESART] = ["next", "prev", "resart"];
+const App = (props) => {
+	const { slides } = props;
 
-const App = () => {
+	const [index, setIndex] = useState(0);
+
+	let title = slides[index].title;
+	let text = slides[index].text;
+
+	function handleClick(clickedButtonName) {
+		let newPos = index;
+		switch (clickedButtonName) {
+			case NEXT:
+				console.log("dealting next...");
+				++newPos;
+				break;
+			case PREV:
+				--newPos;
+				break;
+			case RESART:
+				newPos = 0;
+				break;
+			default:
+				break;
+		}
+		setIndex(newPos);
+	}
+
 	return (
 		<>
-			<MySlide>
-				<ButtonList />
-			</MySlide>
+			<h1 data-testid="title">{title}</h1>
+			<p data-testid="text">{text}</p>
+			<button
+				onClick={() => handleClick(PREV)}
+				data-testid={`button-${PREV}`}
+				disabled={index === 0 ? true : false}>
+				Prev
+			</button>
+			<button
+				onClick={() => handleClick(NEXT)}
+				data-testid={`button-${NEXT}`}
+				disabled={index === slides.length - 1 ? true : false}>
+				Next
+			</button>
+			<button
+				onClick={() => handleClick(RESART)}
+				data-testid={`button-${RESART}`}
+				disabled={index === 0 ? true : false}>
+				Restart
+			</button>
 		</>
 	);
 };
 
 export default App;
+
+//redux - code
+// import React from "react";
+// import MySlide from "./MySlideView/MySlide";
+// import ButtonList from "./ButtonList/ButtonList";
+// import "../styles/App.css";
+
+// const App = () => {
+// 	return (
+// 		<>
+// 			<MySlide>
+// 				<ButtonList />
+// 			</MySlide>
+// 		</>
+// 	);
+// };
+
+// export default App;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,29 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { Provider } from "react-redux";
-import store from "./redux/store";
 import App from "./components/App";
+import slides from "./data.js";
 
-ReactDOM.render(
-	<Provider store={store}>
-		<App />
-	</Provider>,
-	document.getElementById("root")
-);
+ReactDOM.render(<App slides={slides} />, document.getElementById("root"));
+
+// import React from "react";
+// import ReactDOM from "react-dom";
+// import { Provider } from "react-redux";
+// import store from "./redux/store";
+// import { connect } from "./redux/store";
+// import App from "./components/App";
+
+// ReactDOM.render(
+// 	<Provider store={store}>
+// 		<App slides={props.slides}/>
+// 	</Provider>,
+// 	document.getElementById("root")
+// );
+
+// const mapStateToProps = (store) => {
+// 	return {
+// 		slideNo: store.slideReducer.slideNo,
+// 		slides: store.slideReducer.slides
+// 	};
+// };
+
+// export default connect(mapStateToProps, null)(App);
